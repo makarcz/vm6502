174f8ce1a6f0beb7a0630220eab66bb073f4066e
Marek Karcz <makarcz@users.noreply.github.com>
2022-05-09 03:12:46 -0400

    Upgrade mingw compiler
    Upgraded mingw to 8.1.0, added mingw-w64.bat to setup compilation environment / console and updated makefile.mingw.

---
+++ mingw-w64.bat
@@ -0,0 +1,9 @@
+echo off
+set MINGWDIR=C:\mingw-w64\x86_64-8.1.0
+set SDLDIR=D:\src\SDL
+set PATH=%MINGWDIR%\mingw64\bin;%SDLDIR%\lib\x64;%PATH%
+rem echo %PATH%
+rem cd "C:\mingw-w64\x86_64-8.1.0\mingw64\bin"
+D:
+cd "D:\src\githubwrk\vm6502"
+"C:\Windows\system32\cmd.exe"
